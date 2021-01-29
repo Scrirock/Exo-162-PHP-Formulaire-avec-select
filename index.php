bad3441206e78d42d60ed5313b1410d25d85898f
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-01-29 14:11:43 +0100

    PATHINFO

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 
     if (isset($_POST["nom"], $_POST["prenom"], $_POST["sexe"], $_FILES["file"])) {
-        $filename = $_FILES["file"]["name"];
-        $extension=strrchr($filename,'.');
-        $extension=substr($extension,1);
+        $extension = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
         echo $_POST["sexe"]." ".$_POST["nom"] . " " . $_POST["prenom"]." ".$extension;
 
         if ($extension === "pdf") echo "<br>le fichier est bien un pdf";
